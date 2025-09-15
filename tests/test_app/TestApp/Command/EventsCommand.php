66cd50128491d052201927eb0fdaacbd46712fea
ADmad <admad.coder@gmail.com>
2025-09-15 19:08:13 +0530

    Fix argument type after 5.next merge.

--- tests/test_app/TestApp/Command/EventsCommand.php
+++ tests/test_app/TestApp/Command/EventsCommand.php
@@ -5,7 +5,7 @@ namespace TestApp\Command;
 
 use Cake\Command\Command;
 use Cake\Console\Arguments;
-use Cake\Console\ConsoleIo;
+use Cake\Console\ConsoleIoInterface;
 use Cake\Event\EventInterface;
 
 class EventsCommand extends Command
@@ -15,26 +15,20 @@ class EventsCommand extends Command
         return 'This is a command that uses events';
     }
 
-    public function execute(Arguments $args, ConsoleIo $io): ?int
+    public function execute(Arguments $args, ConsoleIoInterface $io): ?int
     {
         $io->out('execute run');
 
         return null;
     }
 
-    public function beforeExecute(EventInterface $event, Arguments $args, ConsoleIo $io): void
+    public function beforeExecute(EventInterface $event, Arguments $args, ConsoleIoInterface $io): void
     {
-        /** @var ConsoleIo $io */
-        $io = $event->getData('io');
-
         $io->out('beforeExecute run');
     }
 
-    public function afterExecute(EventInterface $event, Arguments $args, ConsoleIo $io, mixed $result): void
+    public function afterExecute(EventInterface $event, Arguments $args, ConsoleIoInterface $io, mixed $result): void
     {
-        /** @var ConsoleIo $io */
-        $io = $event->getData('io');
-
         $io->out('afterExecute run');
     }
 }
