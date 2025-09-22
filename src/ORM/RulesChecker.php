23d60dd4b6e8fb98ca28b35c5c57901364532aa0
Daniel <nook24@users.noreply.github.com>
2025-09-22 09:31:43 +0200

    Fix Line exceeds 120 characters; contains 124 characters

--- src/ORM/RulesChecker.php
+++ src/ORM/RulesChecker.php
@@ -92,7 +92,8 @@ class RulesChecker extends BaseRulesChecker
      *
      * @param array<string>|string $field The field or list of fields to check for existence by
      * primary key lookup in the other table.
-     * @param \Cake\ORM\Table|\Cake\ORM\Association|string $associated The alias name of the table where the fields existence will be checked.
+     * @param \Cake\ORM\Table|\Cake\ORM\Association|string $associated The alias name of the table
+     *   where the fields existence will be checked.
      * @param array<string, mixed>|string|null $message The error message to show in case the rule does not pass. Can
      *   also be an array of options. When an array, the 'message' key can be used to provide a message.
      * @return \Cake\Datasource\RuleInvoker
